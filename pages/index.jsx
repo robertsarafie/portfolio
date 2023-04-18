5b01b87c8cac72b8f36051ea75eedd5f9f16d9c6
robertsarafie <sarafierobert@gmail.com>
2023-04-18 17:00:37 +0300

    update v1.3 favicon/title

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,18 @@ import About from '../components/homepage/About';
 import Contact from '../components/homepage/Contact';
 import Skills from '../components/homepage/Skills';
 import Work from '../components/homepage/Work';
+import { Helmet } from 'react-helmet';
 
 export default function Home() {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Robert Sarafie Portfolio</title>
+        <link rel="canonical" href="https://robert-sarafie.netlify.app/" />
+        <meta name="description" content="Robert Sarafie Portfolio" />
+      </Helmet>
+
       <div className="content">
         <div className="content-main">
           <section className="about" id="top">
